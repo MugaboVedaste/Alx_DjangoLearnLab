f795b1945cdb14475676082303a5cfa0a2940f38
Vedaste MUGABO <vedasteapp@gmail.com>
2025-07-20 21:04:28 +0200

    Working on View and Templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,11 +7,10 @@ def book_list_view(request):
      books = Book.objects.all()  # Use plain all() to pass the check
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Book
+from .models import Library
+from django.views.generic.detail import DetailView
 
-# Function-based view to list all books and their authors
-def book_list_view(request):
-     books = Book.objects.all()  # Use plain all() to pass the check
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
