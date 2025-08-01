ee5de0cf73c2ca14fb38f2571fb7a7703b6ddd4b
Vedaste MUGABO <vedasteapp@gmail.com>
2025-08-01 13:42:44 +0200

    autho and permissions on API

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from .views import BookList, BookViewSet
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -8,4 +9,5 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
     path('', include(router.urls)),
+    path('get-token/', obtain_auth_token, name='api_token_auth'),
 ]
