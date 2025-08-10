8ca075a6eb06a7887135e48ef806e6a99853d1c5
Vedaste MUGABO <vedasteapp@gmail.com>
2025-08-10 11:31:08 +0200

    on advamced API view

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),              # GET all books
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'), # GET single book
     path('books/create/', BookCreateView.as_view(), name='book-create'),   # POST create
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'), # PUT/PATCH update
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'), # DELETE book
+    path('books/update/', BookUpdateView.as_view(), name='book-update'), # PUT/PATCH update
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'), # DELETE book
 ]
