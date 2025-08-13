c1495f60dc53b42cb4e7cb96cda8727054280374
Vedaste MUGABO <vedasteapp@gmail.com>
2025-08-13 18:15:48 +0200

    Adding tags and search

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment
+from taggit.forms import TagWidget
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -28,6 +29,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),  # this is the key part
+        }
     tags = forms.CharField(required=False, help_text="Add tags separated by commas")
 class CommentForm(forms.ModelForm):
     class Meta:
