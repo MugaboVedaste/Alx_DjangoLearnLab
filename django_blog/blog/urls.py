ec9329a0a788ea50c3b3c8bac77c7ef4fbed2ba0
Vedaste MUGABO <vedasteapp@gmail.com>
2025-08-13 17:04:17 +0200

    Change in URL edit to update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/comment/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-edit'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-edit'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
     
 ]
